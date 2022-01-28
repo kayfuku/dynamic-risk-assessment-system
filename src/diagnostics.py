88741f7c892ea09c31a965e79b6bd009fc56aff3
Kei Fukutani <kifinewera@gmail.com>
2022-01-28 18:13:05 +0900

    add model_predictions()

--- src/diagnostics.py
+++ src/diagnostics.py
@@ -1,45 +1,64 @@
-
+"""
+Diagnose model and data
+Author: Kei
+Date: January, 2022
+"""
 import pandas as pd
 import numpy as np
 import timeit
 import os
 import json
+import logging
+import pickle
 
-##################Load config.json and get environment variables
-with open('config.json','r') as f:
-    config = json.load(f) 
+from config import TEST_DATA_PATH, PROD_DEPLOYMENT_PATH
 
-dataset_csv_path = os.path.join(config['output_folder_path']) 
-test_data_path = os.path.join(config['test_data_path']) 
+logging.basicConfig(level=logging.INFO)
 
-##################Function to get model predictions
+
 def model_predictions():
-    #read the deployed model and a test dataset, calculate predictions
-    return #return value should be a list containing all predictions
+    """
+    Load test data and deployed model to predict on the test data.
+    """
+    logging.info("Loading and preparing testdata.csv")
+    test_df = pd.read_csv(os.path.join(TEST_DATA_PATH, 'testdata.csv'))
+    X_df = test_df.drop(['corporation', 'exited'], axis=1)
 
-##################Function to get summary statistics
+    logging.info("Loading deployed model")
+    with open(os.path.join(PROD_DEPLOYMENT_PATH, 'trainedmodel.pkl'), 'rb') as f:
+        model = pickle.load(f)
+
+    logging.info("Making predictions on data")
+    y_pred = model.predict(X_df)
+
+    return y_pred
+
+# Function to get summary statistics
+
+
 def dataframe_summary():
-    #calculate summary statistics here
-    return #return value should be a list containing all summary statistics
+    # calculate summary statistics here
+    return  # return value should be a list containing all summary statistics
 
-##################Function to get timings
+# Function to get timings
+
+
 def execution_time():
-    #calculate timing of training.py and ingestion.py
-    return #return a list of 2 timing values in seconds
+    # calculate timing of training.py and ingestion.py
+    return  # return a list of 2 timing values in seconds
 
-##################Function to check dependencies
+# Function to check dependencies
+
+
 def outdated_packages_list():
-    #get a list of 
+    # get a list of
+    pass
 
 
 if __name__ == '__main__':
-    model_predictions()
+    y_pred = model_predictions()
+    print(y_pred)
+
     dataframe_summary()
     execution_time()
     outdated_packages_list()
-
-
-
-
-
-    
