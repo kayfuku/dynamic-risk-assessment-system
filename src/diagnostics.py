b92bbc2446ce7f20faacf5c67e9d1c03ec6fb1b2
Kei Fukutani <kifinewera@gmail.com>
2022-01-28 19:07:30 +0900

    add execution_time()

--- src/diagnostics.py
+++ src/diagnostics.py
@@ -3,6 +3,7 @@ Diagnose model and data
 Author: Kei
 Date: January, 2022
 """
+from textwrap import indent
 from xml.etree.ElementInclude import include
 import pandas as pd
 import numpy as np
@@ -11,6 +12,7 @@ import os
 import json
 import logging
 import pickle
+import subprocess
 
 from config import TEST_DATA_PATH, PROD_DEPLOYMENT_PATH, DATA_PATH
 
@@ -81,12 +83,58 @@ def missing_percentage():
     return missing_data_percentage
 
 
-# Function to get timings
+def _ingestion_timing():
+    """
+    Run ingestion.py script and measures execution time
+
+    Returns:
+        timing: float, running time
+    """
+    starttime = timeit.default_timer()
+    _ = subprocess.run(['python', 'ingestion.py'], capture_output=True)
+    timing = timeit.default_timer() - starttime
+    return timing
+
+
+def _training_timing():
+    """
+    Run training.py script and measures execution time
+
+    Returns:
+        timing: float, running time
+    """
+    starttime = timeit.default_timer()
+    _ = subprocess.run(['python', 'training.py'], capture_output=True)
+    timing = timeit.default_timer() - starttime
+    return timing
 
 
 def execution_time():
-    # calculate timing of training.py and ingestion.py
-    return  # return a list of 2 timing values in seconds
+    """
+    Get average execution time for data ingestion and model training
+    by running 20 times for each.
+
+    Returns:
+        running_time_means: means of execution times for each script
+    """
+    logging.info("Calculating time for ingestion.py")
+    ingestion_time = []
+    for _ in range(20):
+        time = _ingestion_timing()
+        ingestion_time.append(time)
+
+    logging.info("Calculating time for training.py")
+    training_time = []
+    for _ in range(20):
+        time = _training_timing()
+        training_time.append(time)
+
+    running_time_means = [
+        {'ingest_time_mean': np.mean(ingestion_time)},
+        {'train_time_mean': np.mean(training_time)}
+    ]
+
+    return running_time_means
 
 # Function to check dependencies
 
@@ -101,10 +149,12 @@ if __name__ == '__main__':
     print(y_pred)
 
     statistics = dataframe_summary()
-    print(statistics)
+    print(json.dumps(statistics, indent=4))
 
     missing_data_percentage = missing_percentage()
-    print(missing_data_percentage)
+    print(json.dumps(missing_data_percentage, indent=4))
 
-    execution_time()
+    running_time_means = execution_time()
+    print(running_time_means)
+
     outdated_packages_list()
