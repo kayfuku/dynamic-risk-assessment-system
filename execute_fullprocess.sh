7fd02d1f78e4bf21affd1c17fbf52b318a5d9ff6
Kei Fukutani <kifinewera@gmail.com>
2022-01-31 19:43:25 +0900

    update

--- execute_fullprocess.sh
+++ execute_fullprocess.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-conda activate risk_access && \
+conda activate risk_assess && \
 cd /home/workspace/dynamic-risk-assessment-system/src && \
 python fullprocess.py && \
 python apicalls.py
