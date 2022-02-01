2646b1fee9912c8af1679b94c368bd1d75f25515
Kei Fukutani <kifinewera@gmail.com>
2022-02-01 13:46:32 +0900

    update

--- execute_fullprocess.sh
+++ execute_fullprocess.sh
@@ -2,4 +2,4 @@
 source /root/miniconda3/etc/profile.d/conda.sh && \
 conda activate risk-assess && \
 cd /home/workspace/dynamic-risk-assessment-system/src && \
-python apicalls.py
+python fullprocess.py
